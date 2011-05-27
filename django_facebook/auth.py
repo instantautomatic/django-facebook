30ab0511d9150bf819f19fa8ea1339af7143443b
Zach Snow <z@instantautomatic.com>
2011-05-27 12:56:02 -0700

    Making it easy to specify a proxy user; there's probably a better way.

--- django_facebook/auth.py
+++ django_facebook/auth.py
@@ -3,22 +3,32 @@ from django.contrib.auth.models import User
 from django.contrib.auth.backends import ModelBackend
 import facebook
 
+# Grab the user model.
+UserModel = User
+if hasattr(settings, 'FACEBOOK_USER_MODEL'):
+    parts = settings.FACEBOOK_USER_MODEL.split('.')
+    module_name = '.'.join(parts[0:-1]) 
+    
+    module = __import__(module_name)
+    for part in parts[1:]:
+        module = getattr(module, part)
+    UserModel = module
+
 class FacebookBackend(ModelBackend):
     """ Authenticate a facebook user. """
     def authenticate(self, fb_uid=None, fb_graphtoken=None):
         """ If we receive a facebook uid then the cookie has already been validated. """
         if fb_uid:
-            user, created = User.objects.get_or_create(username=fb_uid)
+            user, created = UserModule.objects.get_or_create(username=fb_uid)
             return user
         return None
-
 
 class FacebookProfileBackend(ModelBackend):
     """ Authenticate a facebook user and autopopulate facebook data into the users profile. """
     def authenticate(self, fb_uid=None, fb_graphtoken=None):
         """ If we receive a facebook uid then the cookie has already been validated. """
         if fb_uid and fb_graphtoken:
-            user, created = User.objects.get_or_create(username=fb_uid)
+            user, created = UserModel.objects.get_or_create(username=fb_uid)
             if created:
                 # It would be nice to replace this with an asynchronous request
                 graph = facebook.GraphAPI(fb_graphtoken)
@@ -28,5 +38,6 @@ class FacebookProfileBackend(ModelBackend):
                     if me.get('last_name'): user.last_name = me['last_name']
                     if me.get('email'): user.email = me['email']
                     user.save()
+
             return user
         return None
